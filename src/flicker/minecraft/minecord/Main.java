7849f5e67d87dca330e431e1821544211be59608
Alex231 <a13x231mcsl@gmail.com>
2017-05-25 20:07:04 +0100

    Added Discord libraries. Added Config file.

--- src/flicker/minecraft/minecord/Main.java
+++ src/flicker/minecraft/minecord/Main.java
@@ -2,14 +2,25 @@ package flicker.minecraft.minecord;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import net.dv8tion.jda.*;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
 
 public class Main extends JavaPlugin {
 
+	FileConfiguration config = getConfig();
+	
 	@Override
 	public void onEnable()
 	{
+		config.addDefault("DiscordBotToken", "");
+		config.options().copyDefaults(true);
+		saveConfig();
 		
+		//JDABuilder jda = new JDABuilder(AccountType.BOT).setToken()
 	}
 	
 	@Override
